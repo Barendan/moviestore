78615b51b36213b4845cf09579ab050398d0bf01
barendan <Barendan@gmail.com>
2016-08-31 07:20:59 -0400

    Incorporated devise gem for user functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root 'movies#index'	
   resources :movies, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
