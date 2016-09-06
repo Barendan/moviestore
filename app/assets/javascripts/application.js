8e3cda4f90ab5d033ccf842b5fa9fd1996ec34a2
barendan <Barendan@gmail.com>
2016-09-06 08:02:29 -0400

    Changed header styles and a few other minor styling fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,25 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require turbolinks
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
-
-$(window).load ->
-  $('a[data-target]').click (e) ->
-    e.preventDefault()
-    $this = $(this)
-    if $this.data('target') == 'Add to'
-      url = $this.data('addurl')
-      new_target = "Remove from"
-    else
-      url = $this.data('removeurl')
-      new_target = "Add to"
-    $.ajax url: url, type: 'put', success: (data) ->
-      $('.cart-count').html(data)
-      $this.find('span').html(new_target)
-      $this.data('target', new_target)
-
 
 
